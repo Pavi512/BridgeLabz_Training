7e7b0bcd11d04cf348b8a186660eb3cfc345d3f8
Pavi512 <gondipavithra@gmail.com>
2026-06-21 22:20:38 +0530

    [Pavithra] : Program to print duplicate elements in an array

---
+++ src/com/bridgelabz/arrays/DuplicateElements.java
@@ -0,0 +1,35 @@
+package com.bridgelabz.arrays;
+
+import java.util.HashSet;
+import java.util.Scanner;
+
+public class DuplicateElements {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+
+        int[] arr = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        HashSet<Integer> seen = new HashSet<>();
+        HashSet<Integer> duplicates = new HashSet<>();
+
+        for (int num : arr) {
+
+            if (!seen.add(num)) {
+                duplicates.add(num);
+            }
+        }
+
+        System.out.println("Duplicate Elements:");
+
+        for (int num : duplicates) {
+            System.out.println(num);
+        }
+    }
+}
