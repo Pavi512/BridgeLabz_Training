7e297840e9d00d3ca4d1573fbd300e3af9e1c7ea
Pavi512 <gondipavithra@gmail.com>
2026-06-21 22:21:02 +0530

    [Pavithra] : Program to sort array elements in ascending order

---
+++ src/com/bridgelabz/arrays/SortArray.java
@@ -0,0 +1,37 @@
+package com.bridgelabz.arrays;
+
+import java.util.Scanner;
+
+public class SortArray {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+
+        int[] arr = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        for (int i = 0; i < n - 1; i++) {
+
+            for (int j = i + 1; j < n; j++) {
+
+                if (arr[i] > arr[j]) {
+
+                    int temp = arr[i];
+                    arr[i] = arr[j];
+                    arr[j] = temp;
+                }
+            }
+        }
+
+        System.out.println("Array After Sorting:");
+
+        for (int num : arr) {
+            System.out.print(num + " ");
+        }
+    }
+}
