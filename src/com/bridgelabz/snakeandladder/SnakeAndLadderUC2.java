5ece8d3478c116347316b92d5f68d837bf3e0322
Pavi512 <gondipavithra@gmail.com>
2026-06-22 17:47:35 +0530

    [Pavithra] : Setting the position of user by using Random method

---
+++ src/com/bridgelabz/snakeandladder/SnakeAndLadderUC2.java
@@ -0,0 +1,12 @@
+package com.bridgelabz.snakeandladder;
+
+public class SnakeAndLadderUC2 {
+    public static void main(String[] args) {
+
+        int position = 0;
+
+        int dice = (int)(Math.random() * 6) + 1;
+
+        System.out.println("Dice = " + dice);
+    }
+}
