d8fe351e667093b1a2ccfebe1321f01c237e6b17
Pavi512 <gondipavithra@gmail.com>
2026-06-22 17:47:00 +0530

    [Pavithra] : setting the position of user as 0

---
+++ src/com/bridgelabz/snakeandladder/SnakeAndLadderUC1.java
@@ -0,0 +1,10 @@
+package com.bridgelabz.snakeandladder;
+
+public class SnakeAndLadderUC1 {
+    public static void main(String[] args) {
+
+        int position = 0;
+
+        System.out.println("Player Position = " + position);
+    }
+}
