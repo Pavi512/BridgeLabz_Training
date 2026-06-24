a5075af48f3b1de495495415a0c974c11af7346e
Pavi512 <gondipavithra@gmail.com>
2026-06-24 10:56:50 +0530

    [Pavithra] : program to display and fixed Gambling stake amount and bet amount for each day.

---
+++ src/com/bridgelabz/gambling/UC1GamblingSimulator.java
@@ -0,0 +1,12 @@
+package com.bridgelabz.gambling;
+
+public class UC1GamblingSimulator {
+
+    static final int STAKE = 100;
+    static final int BET = 1;
+
+    public static void main(String[] args) {
+        System.out.println("Stake: $" + STAKE);
+        System.out.println("Bet per Game: $" + BET);
+    }
+}
