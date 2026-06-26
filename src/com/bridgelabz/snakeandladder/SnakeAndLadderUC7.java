fc5d88af4f693ef372071a3f8cf014c56a7611ff
Pavi512 <gondipavithra@gmail.com>
2026-06-26 12:50:28 +0530

    [Pavithra] : Implemented UC7 to update the game with two players

--- src/com/bridgelabz/snakeandladder/SnakeAndLadderUC7.java
+++ src/com/bridgelabz/snakeandladder/SnakeAndLadderUC7.java
@@ -1,11 +1,15 @@
 package com.bridgelabz.snakeandladder;
 
 public class SnakeAndLadderUC7 {
+    static int player1 = 0;
+    static int player2 = 0;
 
-    public static int playTurn(int position) {
+    public static int playTurn(int playerNum) {
 
         int dice = (int)(Math.random() * 6) + 1;
         int option = (int)(Math.random() * 3);
+
+//        System.out.println("Player " + playerNum + " rolled " + dice);
 
         switch(option) {
 
@@ -13,40 +17,66 @@ public class SnakeAndLadderUC7 {
                 break;
 
             case 1:
-                if(position + dice <= 100)
-                    position += dice;
+//                System.out.println("Ladder");
+
+                if(playerNum == 1){
+                    if(player1 + dice <= 100) {
+                        player1 += dice;
+                    }
+//                    System.out.println("Player 1 Position = " + player1);
+                }
+                else{
+                    if(player2 + dice <= 100){
+                        player2 += dice;
+                    }
+//                    System.out.println("Player 2 Position = " + player2);
+                }
                 break;
 
             case 2:
-                position -= dice;
+//                System.out.println("Snake");
 
-                if(position < 0)
-                    position = 0;
+                if(playerNum == 1){
+                    player1 -= dice;
+                    if(player1 < 0){
+                        player1 = 0;
+                    }
+//                    System.out.println("Player 1 Position = " + player1);
+                }
+                else{
+                    player2 -= dice;
+                    if(player2 < 0){
+                        player2 = 0;
+                    }
+//                    System.out.println("Player 2 Position = " + player2);
+                }
+                break;
         }
 
-        return position;
+        return option;
     }
 
     public static void main(String[] args) {
 
-        int player1 = 0;
-        int player2 = 0;
+        int option;
 
         while(player1 < 100 && player2 < 100) {
 
-            player1 = playTurn(player1);
+            do{
+                option = playTurn(1);
+                if(player1 == 100) {
+                    System.out.println("Player 1 Won");
+                    return;
+                }
+            } while (option == 1);
 
-            if(player1 == 100) {
-                System.out.println("Player 1 Won");
-                break;
-            }
-
-            player2 = playTurn(player2);
-
-            if(player2 == 100) {
-                System.out.println("Player 2 Won");
-                break;
-            }
+            do{
+                option = playTurn(2);
+                if(player2 == 100) {
+                    System.out.println("Player 2 Won");
+                    return;
+                }
+            } while (option == 1);
         }
     }
 }
